6c9ce24a98e40a91b9deaa9d3230eda6064dbfd1
ibs12 <ibrahimallahb@gmail.com>
2022-10-04 01:41:35 -0400

    Twitter Part 2

--- twitter_ios_starter/Twitter/HomeTableViewController.swift
+++ twitter_ios_starter/Twitter/HomeTableViewController.swift
@@ -17,15 +17,21 @@ class HomeTableViewController: UITableViewController {
     let myRefreshControl = UIRefreshControl()
     
     
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadTweets()
-        
-        
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
-
+        self.tableView.rowHeight = UITableView.automaticDimension
+        self.tableView.estimatedRowHeight = 150
+        loadTweets()
     }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.loadTweets()
+    }
+    
     
     
     @objc func loadTweets(){
@@ -36,7 +42,7 @@ class HomeTableViewController: UITableViewController {
         let myUrl = "https://api.twitter.com/1.1/statuses/home_timeline.json"
         let myParams = ["count":numberOfTweet]
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
             
             self.tweetArray.removeAll()
             
@@ -64,7 +70,7 @@ class HomeTableViewController: UITableViewController {
         
         let myParams = ["count": numberOfTweet]
         
-        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams, success: { (tweets: [NSDictionary]) in
+        TwitterAPICaller.client?.getDictionariesRequest(url: myUrl, parameters: myParams as [String : Any], success: { (tweets: [NSDictionary]) in
             
             self.tweetArray.removeAll()
             
@@ -91,20 +97,6 @@ class HomeTableViewController: UITableViewController {
         }
     }
     
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
 
     @IBAction func onLogout(_ sender: Any) {
         
@@ -130,6 +122,12 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        
+        
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
         
         
         return cell
